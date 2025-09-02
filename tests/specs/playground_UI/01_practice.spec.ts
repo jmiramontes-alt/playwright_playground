9a23a050987fcacad37ac76350936392392d2ce1
Kenny_Ruiz <ruizk89@gmail.com>
2025-09-02 19:49:03 -0300

    singup practice code

--- tests/specs/playground_UI/01_practice.spec.ts
+++ tests/specs/playground_UI/01_practice.spec.ts
@@ -2,10 +2,11 @@
 //import { TestDataProvider } from '@common/data/providers/test-data-provider';
 //import { SMOKE, UI } from '@tags';
 import {test, expect} from '@playwright/test';
-import fs from 'fs';
+import {faker} from '@faker-js/faker';
 
 import {HomePage} from '../../../src/ui/pages/HomePage';
 import { SignupLoginPage } from '../../../src/ui/pages/SignupLoginPage';
+import { aq } from '@faker-js/faker/dist/airline-CLphikKp';
 
 test.describe('Automation Exercise - Basic User Flow', () => {
   
@@ -26,26 +27,67 @@ test.describe('Automation Exercise - Basic User Flow', () => {
       await expect(signupLoginPage.loginEmail).toBeVisible();
       await expect(signupLoginPage.loginPassword).toBeVisible();
       await expect(signupLoginPage.loginButton).toBeVisible();
-      //await page.context().storageState({ path: 'authState.json' });
-      //fs.writeFileSync('lastUrl.json', JSON.stringify({ url: page.url() }));
     },
   );
 
   //fill in New User Signup!
-//  test.use({storageState: 'authState.json'});
-
   test(
     'Should fill in the data in the New User Signup!',
     async ({page}) =>{
-    //const {url}= JSON.parse(fs.readFileSync('lastUrl.json', 'utf-8'));
     const signupLoginPage = new SignupLoginPage(page);
     const homePage = new HomePage(page);
+    const email = faker.internet.email();
+    const singUpName= page.locator('[data-qa="signup-name"]');
+    const singUpMail = page.locator('[data-qa="signup-email"]');
+    const singUp = page.locator('[data-qa="signup-button"]');
+    const title2 = page.locator('[id="id_gender1"]');
+    const password = page.locator('[data-qa="password"]');
+    //const dayOfBirth = ('');
+    //const monthOfBirth= page.locator('');
+    //const yearOfBirth= page.locator('');
+    const newsletter = page.locator('[id="newsletter"]');
+    const specialOffers = page.locator('[id="optin"]');
+    const firstName = page.locator('[data-qa="first_name"]');
+    const lastName = page.locator('[data-qa="last_name"]');
+    const company = page.locator('[data-qa="company"]');
+    const address = page.locator('[data-qa="address"]');
+    const address2 = page.locator('[data-qa="address2"]');
+    //const country = page.locator('');
+    const state = page.locator('[data-qa="state"]');
+    const city = page.locator('[data-qa="city"]');
+    const zipCode = page.locator('[data-qa="zipcode"]');
+    const mobileNumber = page.locator('[data-qa="mobile_number"]');
+    const createAccount = page.locator('[data-qa="create-account"]');
     await homePage.openSignupLogin();
-    //await page.goto(url);
-    await signupLoginPage.signUp('KennyZ','kenny@test.com' );
+    //await signupLoginPage.signUp('KennyZ','kenny@test.com');
+    await singUpName.fill('Kenny');
+    await singUpMail.fill(email);
+    await singUp.click();
+    await title2.click();
+    await password.fill('test123');
+    await page.selectOption('#days','29');
+    await page.selectOption('#months','8');
+    await page.selectOption('#years','1989');
+    await page.evaluate(() => window.scrollBy(0, 450));
+    await newsletter.check();
+    await specialOffers.check();
+    await firstName.fill('Kenny');
+    await lastName.fill('Ruiz');
+    await company.fill('Eurozona.SA');
+    await address.fill('La Pampa 1385');
+    await address2.fill('Entre migueletes y miñones');
+    await page.selectOption('#country','New Zealand');
+    await state.fill('Arizona');
+    await city.fill('Tucson');
+    await zipCode.fill('85641');
+    await mobileNumber.fill('+541160296372');
+    await createAccount.click();
+    //await expect(page.getByRole('heading', { level: 2 })).toHaveText('Account Created!');
     await page.waitForTimeout(5000);
-    },
+    
+      },
   );
 
 
-});
+
+})
